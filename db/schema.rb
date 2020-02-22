fb30c99c75f7faeb8c8a5405a3c528d8321893c7
Kelvin Wong <kjhwong@gmail.com>
2020-02-22 21:23:21 +0000

    All changes up to 'Register New User' assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200109170819) do
+ActiveRecord::Schema.define(version: 20200222184536) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -34,6 +47,7 @@ ActiveRecord::Schema.define(version: 20200109170819) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
